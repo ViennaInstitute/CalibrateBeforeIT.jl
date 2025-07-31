1c04b36836b54b2a917bfb2b00c62421ef24740a
Oliver Reiter <reiter@wiiw.ac.at>
2025-07-31 14:49:40 +0200

    Initial test/ structure

---
+++ test/params_and_initial_conditions.jl
@@ -0,0 +1,43 @@
+
+import CalibrateBeforeIT as CBit
+
+using Test
+using JLD2
+
+for geo in ["AT"]
+
+    @testset "Calibration calculation: $(geo), 2010" begin
+
+        ## Save calibration data into such a struct
+        struct CalibrationData
+            calibration::Dict{String, Any}
+            figaro::Dict{String, Any}
+            data::Dict{String, Any}
+            ea::Dict{String, Any}
+            max_calibration_date::CBit.DateTime
+            estimation_date::CBit.DateTime
+        end
+
+        reference_calibration_object = load("$(geo)_2010Q1_calibration_object.jld2",
+                                            "reference_calibration_object")
+        (reference_parameters, reference_initial_conditions) =
+            load("$(geo)_2010Q1_parameters_initial_conditions.jld2",
+                 "reference_parameters", "reference_initial_conditions")
+        calibration_date = CBit.DateTime(2010, 03, 31);
+
+        parameters, initial_conditions =
+            CBit.get_params_and_initial_conditions(reference_calibration_object,
+                                                   calibration_date; scale = 1/10000);
+
+        for key in keys(parameters)
+            @test isapprox(reference_parameters[key],
+                           parameters[key], atol = 1e-6, rtol = 1e-6)
+        end
+
+        for key in keys(initial_conditions)
+            @test isapprox(reference_initial_conditions[key],
+                           initial_conditions[key], atol = 1e-6, rtol = 1e-6)
+        end
+    end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+
+import CalibrateBeforeIT as CBit
+
+using Test
+
+# @testset "Calculating initial conditions" begin
+
+include("params_and_initial_conditions.jl")
+
+# end
