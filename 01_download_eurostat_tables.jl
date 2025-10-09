adf0f142988279361b6f9c204aa24734aa033046
Oliver Reiter <reiter@wiiw.ac.at>
2025-10-09 16:25:45 +0200

    Add une_rt_{a,q}_h to the required data
    The *_h datasets are "historical" data reaching back to 1996. We prepend
    these data to our unemployment time series.

--- 01_download_eurostat_tables.jl
+++ 01_download_eurostat_tables.jl
@@ -48,6 +48,82 @@ result_figaro = CBit.combine_figaro_tables(eurostat_path; conn=conn, skip_if_mis
 
 
 ##------------------------------------------------------------
+## For une_rt_a, keep only non-missing entries
+sqlquery = """
+COPY (
+SELECT * FROM '$(pqfile("une_rt_a"))'
+WHERE value IS NOT NULL
+)
+TO '$(pqfile("une_rt_a"))' (FORMAT parquet)
+"""
+execute(conn,sqlquery);
+
+sqlquery = """
+COPY (
+WITH table1 as (
+SELECT * FROM '$(pqfile("une_rt_a"))'
+WHERE value IS NOT NULL
+), table2 as (
+SELECT * FROM '$(pqfile("une_rt_a_h"))'
+WHERE value IS NOT NULL
+)
+SELECT * FROM table1
+UNION
+SELECT * FROM table2
+WHERE NOT EXISTS (
+   SELECT 1
+   FROM table1
+   WHERE table1.time = table2.time
+   AND table1.freq = table2.freq
+   AND table1.age = table2.age
+   AND table1.unit = table2.unit
+   AND table1.sex = table2.sex
+   AND table1.geo = table2.geo
+))
+TO '$(pqfile("une_rt_a"))' (FORMAT parquet)
+"""
+res_une_rt_a = execute_debug(conn,sqlquery);
+
+
+##------------------------------------------------------------
+## For une_rt_q, keep only non-missing entries
+sqlquery = """
+COPY (
+SELECT * FROM '$(pqfile("une_rt_q"))'
+WHERE value IS NOT NULL
+)
+TO '$(pqfile("une_rt_q"))' (FORMAT parquet)
+"""
+execute(conn,sqlquery);
+
+sqlquery = """
+COPY (
+WITH table1 as (
+SELECT * FROM '$(pqfile("une_rt_q"))'
+WHERE value IS NOT NULL
+), table2 as (
+SELECT * FROM '$(pqfile("une_rt_q_h"))'
+WHERE value IS NOT NULL
+)
+SELECT * FROM table1
+UNION
+SELECT * FROM table2
+WHERE NOT EXISTS (
+   SELECT 1
+   FROM table1
+   WHERE table1.time = table2.time
+   AND table1.freq = table2.freq
+   AND table1.age = table2.age
+   AND table1.unit = table2.unit
+   AND table1.sex = table2.sex
+   AND table1.geo = table2.geo
+   AND table1.s_adj = table2.s_adj
+))
+TO '$(pqfile("une_rt_q"))' (FORMAT parquet)
+"""
+res_une_rt_q = execute_debug(conn,sqlquery);
+
+##------------------------------------------------------------
 ## Step 4: Create bd_9ac_l_form_a64 from bd_9ac_l_form_r2
 success, rows = CBit.create_business_demographic_a64_data("bd_9ac_l_form_r2",
     eurostat_path, conn)
